b31dd6b2e319717efb2e4500f6e53d2be94ce195
Infernet <sborisenko.infernet@gmail.com>
2019-09-03 10:57:20 +0300

    sequelize one to many, many to many fixed config.json

--- src/Sequelize.js
+++ src/Sequelize.js
@@ -1,105 +1,233 @@
 const express = require('express');
-const Sequelize=require('sequelize');
-const bodyParser=require('body-parser');
+const Sequelize = require('sequelize');
+const bodyParser = require('body-parser');
 
 const app = express();
-const urlEndcodedParse = bodyParser.urlencoded({ extended: false });
-const sequelize=new Sequelize('blog_db','infernet','admin',{
-    dialect:'mysql',
-    host:'localhost',
+const urlEndcodedParse = bodyParser.urlencoded({extended: false});
+app.set('view engine', 'hbs');
+
+
+const sequelize = new Sequelize('dev_db', 'infernet', 'admin', {
+    dialect: 'mysql',
+    host: 'localhost',
     define: {
         timestamps: false
     }
 });
-
-const User=sequelize.define('user',{
-   id:{
-       type: Sequelize.INTEGER,
-       autoIncrement:true,
-       primaryKey:true,
-       allowNull:false
-   },
-    name:{
-       type:Sequelize.STRING,
+const User = sequelize.define('user', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
         allowNull: false
     },
-    age:{
-       type:Sequelize.INTEGER,
-        allowNull:false
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    age: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    }
+});
+//one to many
+const Product = sequelize.define('product', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+    },
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    price: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    }
+});
+const Company = sequelize.define('company', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+    },
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
+Company.hasMany(Product, {onDelete: "cascade"});
+
+//many to many
+const Student = sequelize.define('student', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
+const Course = sequelize.define('course', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
     }
 });
 //
-// sequelize.sync({force: true}).then(result=>{
-//     console.log(result);
-// })
-//     .catch(error=>{
-//         console.error(error);
-//     });
-//
-//
-app.use('/static',express.static("./public"));
-
-app.get('/create',(req,res)=>{
-   res.redirect('/static/sequelize.html');
+const Enrolment = sequelize.define('enrolment', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    grade: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    }
 });
 
-app.get('/',(req,res)=>{
-    User.findAll({raw:true})
-        .then(users=>{
-            let body="<!DOCTYPE html>" +
-                "<html lang=\"en\">" +
-                "<head>" +
-                "    <meta charset=\"UTF-8\">" +
-                "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">" +
-                "    <meta http-equiv=\"X-UA-Compatible\" content=\"ie=edge\">" +
-                "    <title>Title</title>" +
-                "</head>" +
-                "<body>"+
-                "<a href='/create'>Добавить запись</a>"+
-                "<table><thead><th>ID</th><th>Name</th> <th>Age</th></thead>";
-            users.forEach((user)=>{
-                body+=`<tr><td>${user.id}</td><td>${user.name}</td><td>${user.age}</td></tr>`;
-            });
-            body+="</table></body></html>";
-            res.status(200).send(body);
+Student.belongsToMany(Course, {through: Enrolment});
+Course.belongsToMany(Student, {through: Enrolment});
+
+app.get('/', (req, res) => {
+    Student.findOne({where: {name: "Tom"}})
+        .then(student => {
+            student.getCourses()
+                .then(courses => {
+                    for (course of courses) {
+                        if (course.name == "JavaScript")
+                            course.enrolment.destroy();
+                    }
+                    res.sendStatus(200);
+                })
         })
-        .catch(error=>{
-            res.sendStatus(400);
+});
+app.get('/test', (req, res) => {
+    Student.findOne({where: {name: "Tom"}})
+        .then(student => {
+            student.getCourses()
+                .then(courses => {
+                    let result = `<html><head><meta charset="UTF-8"></head><body>` +
+                        `<h3>Sudent: ${student.name}</h3>`;
+                    courses.forEach(course => {
+                        result += `<p>Id: ${course.id}</p>` +
+                            `<p>Name: ${course.name}</p>` +
+                            `<p>Grade: ${course.enrolment.grade}</p>`;
+                    });
+                    result += `</body></html>`;
+                    res.send(result);
+                })
+        })
+});
+
+app.get('/:id', (req, res) => {
+    Company.findByPk(req.params.id)
+        .then(company => {
+            let result = `<html><head><meta charset="UTF-8"></head><body>`;
+            company.getProducts()
+                .then(data => {
+                    data.forEach(e => {
+                        result += `<p>Id: ${e.id}</p>` +
+                            `<p>Name: ${e.name}</p>` +
+                            `<p>Company: ${company.name}</p>`
+                            + '<br><br>';
+                    })
+                    result += `</body></html>`;
+                    res.send(result);
+                });
+        })
+        .catch(reason => res.sendStatus(400));
+});
+//
+// //select
+// app.get('/', (req, res) => {
+//     User.findAll({raw: true})
+//         .then(data => {
+//             res.render('index.hbs', {
+//                 users: data
+//             })
+//         })
+//         .catch(error => {
+//             console.log(error);
+//             res.sendStatus(400);
+//         });
+// });
+// //insert
+// app.get('/create', (req, res) => {
+//     res.render('create.hbs')
+// });
+// app.post('/create', urlEndcodedParse, (req, res) => {
+//     if (!req.body)
+//         return res.sendStatus(400);
+//     User.create({name: req.body.name, age: req.body.age})
+//         .then(resolve => {
+//             res.redirect('/');
+//         })
+//         .catch(reason => {
+//             console.log(reason);
+//             res.sendStatus(400);
+//         });
+// });
+// //select for update
+// app.get('/edit/:id', (req, res) => {
+//     let userId = req.params.id;
+//     User.findAll({where: {id: userId}, raw: true})
+//         .then(data => {
+//             res.render('edit.hbs', {
+//                 user: data[0]
+//             });
+//         })
+//         .catch(reason => {
+//             console.log(reason);
+//             res.sendStatus(400);
+//         });
+// });
+// //update
+// app.post('/edit', urlEndcodedParse, (req, res) => {
+//     if (!req.body)
+//         return res.sendStatus(400);
+//     User.update({name: req.body.name, age: req.body.age}, {where: {id: req.body.id}})
+//         .then(resolve => {
+//             res.redirect('/');
+//         })
+//         .catch(reason => {
+//             console.log(reason);
+//             res.sendStatus(400);
+//         });
+// });
+// //delete
+// app.post('/delete/:id', (req, res) => {
+//     User.destroy({where: {id: req.params.id}})
+//         .then(resolve => res.redirect('/'))
+//         .catch(reason => {
+//             console.log(reason);
+//             res.sendStatus(400);
+//         });
+// });
+
+sequelize.sync({force: false})
+    .then(result => {
+        console.log(result);
+        app.listen(process.env.PORT, process.env.HOST, () => {
+            console.log('Server mysql&express ready http://' + process.env.HOST + ':' + process.env.PORT);
         });
-});
-
-app.post('/create',urlEndcodedParse,(req,res)=>{
-    if(!req.body)
-        return res.sendStatus(400);
-    User.create({
-        'name':req.body.name,
-        'age':req.body.age
     })
-        .then(result=>{
-            res.redirect('/');
-        })
-        .catch(error=>{
-            console.log(error);
-            res.status(400).send('Ошибка при попытке добавления нового пользователя'+
-             `{name: ${req.body.name},age:${req.body.age}}`);
-        })
-});
+    .catch(error => {
+        console.error(error);
+    });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(process.env.PORT, process.env.HOST, () => {
-    console.log('Server mysql&express ready http://' + process.env.HOST + ':' + process.env.PORT);
-});
